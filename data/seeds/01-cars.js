9125c759add62c6c85f1e4722ec6b05095bd7ed1
Joe Hebert <raphaelhebert18@gmail.com>
2021-09-07 23:18:36 +0200

    stretch

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,43 @@
-// STRETCH
+const cars = [
+{
+    vin:'1N4AL11D45N487738',
+    make:'tesla',
+    model: 'modelS',
+    mileage: 1234403,
+    title:'clean',
+    transmission: "auto"
+},
+{
+    vin:'1N4AL11D45N487738',
+    make:'tesla',
+    model: 'modelS',
+    mileage: 234433,
+},
+{
+    vin:'5FRYD4H46FB086879',
+    make:'tesla',
+    model: 'modelS',
+    mileage: 1004433,
+    transmission: "manual"
+},
+{
+    vin:'JTKDE177250082874',
+    make:'toyota',
+    model: 'prius',
+    mileage: 1234433,
+    title:'clean',
+    transmission: "manual"
+},
+{
+    vin:'2T3RFREV7DW158576',
+    make:'ford',
+    model: 'focus',
+    mileage: 123334433,
+    transmission: "manual"
+}]
+
+module.send = async function(knex){
+    await knex("cars").truncate()
+    await knex("cars").insert(cars)
+}
+
