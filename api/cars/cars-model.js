4b5588ed93ee8fc5784a8c875bce684edb331d58
Joe Hebert <raphaelhebert18@gmail.com>
2021-09-07 21:02:22 +0200

    completed cars-model

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,13 +1,23 @@
 const db = require('../../data/db-config')
 
 const getAll = () => {
+  //SELECT * FROM cars
   return db('cars')
 }
 
-const getById = () => {
-  // DO YOUR MAGIC
+const getById = (id) => {
+  //SELECT * FROM cars WHERE id = 1
+  return db('cars').where({ id: id })
 }
 
-const create = () => {
-  // DO YOUR MAGIC
+const create = (cars) => {
+  //INSERT INTO cars (vin, make, model, mileage, title, transmission)
+  //VALUES ( 'xxx', 'xxx', 'xxx', 123, 'xxx', 'xxx')
+  db('cars').create(cars)
+}
+
+module.exports = {
+  getAll,
+  getById,
+  create
 }
