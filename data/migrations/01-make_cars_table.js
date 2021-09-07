d17f541116ac06b34a026ff29c08cfbe55510603
Joe Hebert <raphaelhebert18@gmail.com>
2021-09-07 20:57:45 +0200

    migration file completed

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,16 @@
-exports.up = function (knex) {
-  // DO YOUR MAGIC
+exports.up =  async function (knex) {
+  knew.schema.createTable('cars', table => {
+    table.increment()
+    table.string('vin', 128).notNullable().unique()
+    table.string('make', 128).notNullable()
+    table.integer('mileage').notNullable()
+    table.string('title')
+    table.string('transmission')
+  })
+}
+
+exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists('cars')
 };
 
-exports.down = function (knex) {
-  // DO YOUR MAGIC
-};
+
